3d67d6715a561637917d547ebf6204dd2eeb8609
Jeffrey <bernardusjeffrey747@gmail.com>
2017-03-27 10:26:31 +0700

    Generate Route Class

---
+++ transjakarta_/src/transjakarta_/generateRoute.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transjakarta_;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class generateRoute {
+    private generateRoute fastest = new generateRoute();
+    private generateRoute shortest = new generateRoute();
+    
+    public boolean doWeHaveSameStops(int a, int b){
+        
+        return false;
+    }
+    
+    
+    
+}
