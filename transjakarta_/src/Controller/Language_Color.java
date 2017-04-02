62dabc7a26a1e4b92064733e63f033f0ccd6f64e
mikha <mikhayupikha@yahoo.com>
2017-04-02 20:25:57 +0700

    mvc attempt

---
+++ transjakarta_/src/Controller/Language_Color.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author Mikha Putri
+ */
+public interface Language_Color {
+    public void changeToIndo();
+    public void changeToEng();
+    public void changePink();
+    public void changeGray();
+    public void changeDefault();
+}
---
+++ transjakarta_/src/Model/ConnectionConfig.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Mikha Putri
+ */
+public class ConnectionConfig {
+        public static Connection con;
+    
+    public static Connection createConnection() throws ClassNotFoundException{
+        try {
+            if(con==null){
+                Class.forName("com.mysql.jdbc.Driver");
+                String url = "jdbc:mysql://localhost:3306/transjakarta", user = "root", password = "";
+                con = DriverManager.getConnection(url, user, password);
+            }
+            else{
+                //System.out.print("connection has been created");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(transjakarta_.ConnectionConfig.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+    
+    public static void closeConnection()
+    {
+        try{
+          if(con!=null)
+          {
+              con.close();
+          }
+        }catch(SQLException ex){
+            System.out.print(ex);
+        }
+        
+    }
+    
+}
